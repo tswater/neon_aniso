0fc9c8fe3ba1196847f1d035c7f4a8eec1a5209d
Tyler Waterman <tyswater@gmail.com>
2025-07-05 12:00:36 -0600

    curvefitting

--- templates/L2_curvefit.py
+++ templates/L2_curvefit.py
@@ -29,6 +29,66 @@
 # The user defines a list of parameters to fit (i.e. [a0,a2,b0,b1,b2]) and sets any constants (c=1,d=2,e=1).
 # The parameters are then fit across the whole data at once.
 
+# %%
+from neonutil.curvefit import Cfit,load_fit,compare_fits
+from neonutil.nutil import nscale,get_phi,get_phio
+
+# %% [markdown]
+# ## Load in the Data
+# First load in the data to fit 
+
+# %%
+l2file=''
+fp=h5py.File(l2file,'r') # start with open for readonly
+fpsites=fp['main/data']['SITE'][:]
+zL=fp['main/data']['zL'][:]
+ani=fp['main/data']['ANI_YB'][:]
+phi=get_phi(fp['main/data'],'UU')
+
+# %% [markdown]
+# ## How To Run a Full Case
+# First set the input values as follows:
+
+# %%
+base='cbase' # this is the base function; see neonutil.curvefit.py for details/options
+             # You will often want 'cbase', which is cbase(...)=a*(b+c*zL)**(d)+e
+
+const={'a':None,'b':1,'c':-3,'d':1/3,'e':0} # for the parameters in 'base' set their
+                                            # values. "None" means you will fit to this parameter
+
+params=['a0','a1'] # params to fit; in cbase, a(yb)=a0+a1(yb)**1+a2(yb)**2+...
+
+p0=[2.55,0] # list of initial guesses for parameter values; one for each "None" in const
+
+bnds = [.1,-5],[10,5]) # bounds for parameters. Form ([MIN1,MIN2,...],[MAX1,MAX2,...])
+
+loss = 'cauchy' # loss function. For options see scipy.optimize.least_squares documentation for options
+                # cauchy is good default for many outliers; arctan as well. linear sometimes if 
+                # there are few outliers
+
+name='test_full' # name for your fit
+
+var = 'UU' # variable to fit; in this case sigma U
+
+stab = False # if true, is stable conditions, if false, unstable conditions.
+
+# %%
+#### Create Cfit Object
+cf=Cfit(name,var,stab,'full',base,p0=p0,const=const,bnds=bnds,loss=loss,params=params)
+
+# %%
+#### Fit data
+cf.cfit(zL,ani,phi)
+
+# %%
+#### Evaluate the fit
+cf.eval_cfit(zL,ani,phi,fpsites)
+compare_fits([cf]) # show the fit performance
+
+# %%
+#### Save the fit 
+cf.save_fit(fp['main'])
+
 # %% [markdown]
 # ## How To Run a Two Stage Case
 # First set the input values as follows:
@@ -53,10 +113,27 @@ deg=[1] # list of polynomial degrees for each parameter. "1" means there will be
 typ='lin' # function applied to yb before fit. Can be 'lin' or 'log'. Log will make params
           # a function of np.log10(yb) instead of (yb)
 
-name='test' # name for your fit
+name='test_2stage' # name for your fit
 
 var = 'UU' # variable to fit; in this case sigma U
 
 stab = False # if true, is stable conditions, if false, unstable conditions.
+
+# %%
+#### Create Cfit Object
+cf=Cfit(name,var,stab,'2_stage',base,p0=p0,const=const,bnds=bnds,loss=loss,deg=deg,typ=typ)
+
+# %%
+#### Fit data
+cf.cfit(zL,ani,phi)
+
+# %%
+#### Evaluate the fit
+cf.eval_cfit(zL,ani,phi,fpsites)
+compare_fits([cf]) # show the fit performance
+
+# %%
+#### Save the fit 
+cf.save_fit(fp['main'])
 
 # %%
